89fe6f66320227f20aa081838af9bc14cf38d84d
ThienCuong <thiencuong1903@gmail.com>
2026-04-07 19:45:13 +0700

    initial commit

---
+++ src/main/java/com/group26/smart_home_system/SmartHomeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group26.smart_home_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeSystemApplication.class, args);
+	}
+
+}
