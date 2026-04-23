6a04031d0af55ad025c117f19b91187a0dae9d18
ThienCuong <thiencuong1903@gmail.com>
2026-04-23 09:23:27 +0700

    fix: add missing @RequestBody annotation in ScheduleController

--- src/main/java/com/group26/smart_home_system/controller/ScheduleController.java
+++ src/main/java/com/group26/smart_home_system/controller/ScheduleController.java
@@ -9,11 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/schedules")
@@ -25,7 +21,7 @@ public class ScheduleController {
   @PreAuthorize("hasAnyRole('USER')")
   @PostMapping
   public ResponseEntity<ScheduleResponse> createSchedule(
-      CreateScheduleRequest createScheduleRequest)
+      @RequestBody CreateScheduleRequest createScheduleRequest)
       throws ActuatorNotFoundException {
     ScheduleResponse scheduleResponse = scheduleService.createSchedule(createScheduleRequest);
     return ResponseEntity.status(HttpStatus.CREATED).body(scheduleResponse);
