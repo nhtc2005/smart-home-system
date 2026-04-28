3ab87906c22dc492de771480eb477a34167085b1
ThienCuong <thiencuong1903@gmail.com>
2026-04-28 08:38:31 +0700

    feat: add CORS configuration

--- src/main/java/com/group26/smart_home_system/security/SecurityConfig.java
+++ src/main/java/com/group26/smart_home_system/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.group26.smart_home_system.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,14 +23,24 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-  private final String[] PUBLIC_ENDPOINTS = {"/swagger-ui/**", "/v3/api-docs/**",
-      "/swagger-ui.html", "/auth/register", "/auth/login", "/ws/**", "/api/ws/**"};
+  private final String[] PUBLIC_ENDPOINTS = {
+    "/swagger-ui/**",
+    "/v3/api-docs/**",
+    "/swagger-ui.html",
+    "/auth/register",
+    "/auth/login",
+    "/ws/**",
+    "/api/ws/**"
+  };
 
   @Value("${jwt.secret}")
   private String secretKey;
@@ -46,12 +57,10 @@ public class SecurityConfig {
   public JwtDecoder jwtDecoder(TokenValidationService tokenValidationService) {
     SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HS512");
 
-    NimbusJwtDecoder nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
-        .macAlgorithm(MacAlgorithm.HS512)
-        .build();
+    NimbusJwtDecoder nimbusJwtDecoder =
+        NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
 
     return token -> {
-
       Jwt jwt = nimbusJwtDecoder.decode(token);
 
       if (tokenValidationService.isBlacklisted(jwt.getId())) {
@@ -64,7 +73,8 @@ public class SecurityConfig {
 
   @Bean
   public JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+        new JwtGrantedAuthoritiesConverter();
     jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
 
     JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
@@ -76,41 +86,64 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(
       HttpSecurity httpSecurity,
       JwtDecoder jwtDecoder,
-      CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
-    httpSecurity.csrf(AbstractHttpConfigurer::disable)
-        .cors(cors -> {
-        })
-        .authorizeHttpRequests(request -> request
-            .requestMatchers(PUBLIC_ENDPOINTS)
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-        )
-        .oauth2ResourceServer(oauth2 -> oauth2
-            .jwt(jwt -> jwt
-                .decoder(jwtDecoder)
-                .jwtAuthenticationConverter(jwtAuthenticationConverter())
-            )
-            .authenticationEntryPoint(customAuthenticationEntryPoint)
-        )
-        .exceptionHandling(exception -> exception
-            .authenticationEntryPoint(customAuthenticationEntryPoint)
-            .accessDeniedHandler((request, response, ex) -> {
-              response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-              response.setContentType("application/json");
+      CustomAuthenticationEntryPoint customAuthenticationEntryPoint)
+      throws Exception {
+    httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> {})
+        .authorizeHttpRequests(
+            request ->
+                request.requestMatchers(PUBLIC_ENDPOINTS).permitAll().anyRequest().authenticated())
+        .oauth2ResourceServer(
+            oauth2 ->
+                oauth2
+                    .jwt(
+                        jwt ->
+                            jwt.decoder(jwtDecoder)
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                    .authenticationEntryPoint(customAuthenticationEntryPoint))
+        .exceptionHandling(
+            exception ->
+                exception
+                    .authenticationEntryPoint(customAuthenticationEntryPoint)
+                    .accessDeniedHandler(
+                        (request, response, ex) -> {
+                          response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                          response.setContentType("application/json");
 
-              Map<String, Object> body = Map.of(
-                  "message", ex.getMessage(),
-                  "error", "403 FORBIDDEN",
-                  "status", 403,
-                  "timestamp", Instant.now()
-              );
+                          Map<String, Object> body =
+                              Map.of(
+                                  "message",
+                                  ex.getMessage(),
+                                  "error",
+                                  "403 FORBIDDEN",
+                                  "status",
+                                  403,
+                                  "timestamp",
+                                  Instant.now());
 
-              response.getWriter().write(new ObjectMapper().writeValueAsString(body));
-            })
-        );
+                          response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+                        }));
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+    corsConfiguration.setAllowedHeaders(List.of("*"));
+    corsConfiguration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+    return urlBasedCorsConfigurationSource;
   }
 
 }
